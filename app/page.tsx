dc856a70c1ccfa222ebf1d029c42865d12c599b6
Alimjan2013 <alimujiang1414@qq.com>
2023-06-24 20:17:46 +0800

    style: modify for phone

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default async function Home() {
   }
   const wordList = await getData();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between md:p-24 p-8">
       <Dictation wordList={wordList} />
     </main>
   );
