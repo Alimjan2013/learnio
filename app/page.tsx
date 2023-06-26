fa89981a746258d0e05692a3e8acf810506864c2
Alimjan2013 <alimujiang1414@qq.com>
2023-06-26 19:34:30 +0800

    fix:Type error

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between md:p-24 p-8">
       <AuthForm />
-      <Dictation />
     </main>
   );
 }
