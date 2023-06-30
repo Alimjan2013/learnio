1e3e872b69d9f6dd3b441d86c7c8d861927abd24
Alimjan2013 <alimujiang1414@qq.com>
2023-06-30 22:08:59 +0800

    feat: nevagation for dictation list

--- app/dictation/page.tsx
+++ app/dictation/page.tsx
@@ -1,15 +1,17 @@
-import Dictation from "@/components/tool/dictation";
+import { kv } from "@vercel/kv";
+import Link from "next/link";
 
 export default async function DictationPage() {
   // const wordList = await getData();
+  const wordListIndex = (await kv.get("wordListIndex")) as Array<string>;
+  console.log(typeof wordListIndex);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between md:p-24 p-8">
-      {/* <Dictation /> */}
-      <form action="/auth/signout" method="post">
-        <button className="button block" type="submit">
-          Sign out
-        </button>
-      </form>
+      {wordListIndex.map((item) => (
+        <Link key={item} href={`dictation/pages/${item}`}>
+          章节{item}
+        </Link>
+      ))}
     </main>
   );
 }
