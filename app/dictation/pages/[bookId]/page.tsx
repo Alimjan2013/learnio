89111eeead5b39725704e57873b4542d5f9a8c3e
Alimjan2013 <alimujiang1414@qq.com>
2023-07-03 21:18:23 +0800

    feat: back to menu

--- app/dictation/pages/[bookId]/page.tsx
+++ app/dictation/pages/[bookId]/page.tsx
@@ -1,4 +1,7 @@
 import Dictation from "@/components/tool/dictation";
+import Link from "next/link";
+import { ChevronLeft } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 export default async function DictationPage({
   params,
@@ -6,7 +9,15 @@ export default async function DictationPage({
   params: { bookId: string };
 }) {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between md:p-24 p-8">
+    <main className="flex min-h-screen flex-col items-center space-y-4 md:p-24 p-8">
+      <div className="w-full flex">
+        <Link href="/dictation">
+          <Button variant="outline">
+            <ChevronLeft className="mr-2 h-4 w-4" /> back
+          </Button>
+        </Link>
+      </div>
+
       <Dictation id={params.bookId} />
     </main>
   );
