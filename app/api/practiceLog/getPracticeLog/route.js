366cb522a642c8713f6c07203b86366ea4854e38
Alimjan2013 <alimujiang1414@qq.com>
2023-09-12 21:31:45 +0800

    feat: get 10 days records

--- app/api/practiceLog/getPracticeLog/route.js
+++ app/api/practiceLog/getPracticeLog/route.js
@@ -5,10 +5,16 @@ export async function POST(request) {
   const supabase = createClientComponentClient();
 
   const res = await request.json();
+  const today = new Date();
+
+  const tenDaysAgo = new Date(today.getTime() - 10 * 24 * 60 * 60 * 1000);
+
   const { data, error } = await supabase
     .from("practiceLog")
     .select("*")
-    .eq("user_id", res.userId);
+    .eq("user_id", res.userId)
+    .gt("created_at", tenDaysAgo.toISOString());
+
   // console.log(data);
 
   return NextResponse.json(data);
